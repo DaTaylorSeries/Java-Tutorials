96b20596518f39c6a5820ef139c8daa5bbe294b1
DaTaylorSeries <zippytato@gmail.com>
2021-03-25 13:25:08 -0600

    3/25/21: Commit

---
+++ Mouse Listener/src/Main/MyFrame.java
@@ -0,0 +1,67 @@
+package Main;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MyFrame extends JFrame implements MouseListener {
+    
+    JLabel label;
+    ImageIcon smile;
+    ImageIcon nervous;
+    ImageIcon pain;
+    ImageIcon dizzy;
+    
+    
+    MyFrame() {
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(new FlowLayout());
+        
+        label = new JLabel();
+        label.addMouseListener(this);
+        
+        smile = new ImageIcon("smile.png");
+        nervous = new ImageIcon("nervous.png");
+        pain = new ImageIcon("pain.png");
+        dizzy = new ImageIcon("dizzy.png");
+        
+        label.setIcon(smile);
+        this.add(label);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+          label.setIcon(pain);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+          label.setIcon(dizzy);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+          label.setIcon(nervous);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+          label.setIcon(smile);
+    }
+    
+}
